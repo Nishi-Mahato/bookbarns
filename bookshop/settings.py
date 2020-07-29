33f110c06bbb0378c24ce897c04b97e53c6b7739
Nishi-Mahato <54651764+Nishi-Mahato@users.noreply.github.com>
2020-07-29 18:04:32 -0500

    Update settings.py

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -168,14 +168,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 CART_SESSION_ID = 'cart'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'nishimahto@gmail.com'
-EMAIL_HOST_PASSWORD = 'fhyrxccdliqldtsx'
-
-
 
 # Braintree settings
 BRAINTREE_MERCHANT_ID = 'b9pnpp9rpbx6gfwp'
